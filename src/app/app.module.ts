afb4eff0c4497e7b12e249718b880960e2b4737b
Obtez <contact.davidbeer@gmail.com>
2022-04-03 00:19:31 +0200

    automatic update of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './features/start-screen/start-screen.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { RecruitmentComponent } from './features/recruitment/recruitment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartScreenComponent,
-    DashboardComponent
+    DashboardComponent,
+    RecruitmentComponent
   ],
   imports: [
     BrowserModule,
