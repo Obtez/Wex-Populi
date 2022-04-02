23478701425f8430556af1b12374f6c491591fb9
Obtez <contact.davidbeer@gmail.com>
2022-04-03 00:36:26 +0200

    added path for recruitment component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {StartScreenComponent} from "./features/start-screen/start-screen.component";
 import {DashboardComponent} from "./features/dashboard/dashboard.component";
+import {RecruitmentComponent} from "./features/recruitment/recruitment.component";
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'recruitment', component: RecruitmentComponent }
 ];
 
 @NgModule({
