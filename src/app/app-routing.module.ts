e053d2584522580d02d708620f1d20a880e96483
Obtez <contact.davidbeer@gmail.com>
2022-04-02 23:55:32 +0200

    added routes for "/" and "/dashboard"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartScreenComponent} from "./features/start-screen/start-screen.component";
+import {DashboardComponent} from "./features/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartScreenComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
