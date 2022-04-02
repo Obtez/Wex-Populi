8490c2685aa1ef52aa15549abc08c6d64a8dab3a
Obtez <contact.davidbeer@gmail.com>
2022-04-02 23:55:51 +0200

    automatic module additions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './features/start-screen/start-screen.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
