c3cdf9692cc97cf9e1bc7b2854b36661cbbf63f1
Obtez <contact.davidbeer@gmail.com>
2022-04-03 00:43:55 +0200

    created some basic test cases

--- src/app/features/recruitment/recruitment.service.spec.ts
+++ src/app/features/recruitment/recruitment.service.spec.ts
@@ -13,4 +13,19 @@ describe('RecruitmentService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create an instance of fighterType "grunt"', () => {
+    const result = service.recruitNewFighter('grunt');
+    expect(result.fighterType).toEqual('Grunt');
+  })
+
+  it('should create an instance of fighterType "Specialist"', () => {
+    const result = service.recruitNewFighter('specialist');
+    expect(result.fighterType).toEqual('Specialist');
+  })
+
+  it('should create an instance of fighterType "Commander"', () => {
+    const result = service.recruitNewFighter('commander');
+    expect(result.fighterType).toEqual('Commander');
+  })
 });
