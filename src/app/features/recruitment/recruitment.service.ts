67e636626addd1003a2cfdb0b179225b2eecf425
Obtez <contact.davidbeer@gmail.com>
2022-04-03 18:53:40 +0200

    old API - will be changed soon

--- src/app/features/recruitment/recruitment.service.ts
+++ src/app/features/recruitment/recruitment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Grunt} from "../../common/character/grunt";
 import {Specialist} from "../../common/character/specialist";
 import {Commander} from "../../common/character/commander";
+import {RecruitmentApiService} from "../../common/services/recruitment-api.service";
 
 export type FighterType = 'grunt' | 'specialist' | 'commander';
 
@@ -10,24 +11,33 @@ export type FighterType = 'grunt' | 'specialist' | 'commander';
 })
 export class RecruitmentService {
 
-  constructor() { }
+  constructor(
+    private recruitmentAPI: RecruitmentApiService
+  ) { }
 
   public recruitNewFighter(type: FighterType) {
     switch(type) {
       case 'grunt':
-        return RecruitmentService.recruitGrunt();
+        const newGrunt: Grunt = RecruitmentService.recruitGrunt();
+        this.recruitmentAPI.addRecruit(type, newGrunt);
+        break;
 
       case 'specialist':
-        return RecruitmentService.recruitSpecialist();
+        const newSpecialist = RecruitmentService.recruitSpecialist();
+        this.recruitmentAPI.addRecruit(type, newSpecialist);
+        break;
 
       case 'commander':
-        return RecruitmentService.recruitCommander();
+        const newCommander = RecruitmentService.recruitCommander();
+        this.recruitmentAPI.addRecruit(type, newCommander);
+        break;
 
       default:
         throw new Error('There was an error recruiting a new fighter')
     }
   }
 
+  // TODO: Currently overwrites db
   private static recruitGrunt(): Grunt {
     return new Grunt();
   }
