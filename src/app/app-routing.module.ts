cdd5419a68dff8fd930c06d7d97a062729fd870f
Obtez <contact.davidbeer@gmail.com>
2022-04-03 17:29:13 +0200

    new route: /factions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {StartScreenComponent} from "./features/start-screen/start-screen.component";
 import {DashboardComponent} from "./features/dashboard/dashboard.component";
 import {RecruitmentComponent} from "./features/recruitment/recruitment.component";
+import {FactionsComponent} from "./features/factions/factions.component";
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'recruitment', component: RecruitmentComponent }
+  { path: 'recruitment', component: RecruitmentComponent },
+  { path: 'factions', component: FactionsComponent }
 ];
 
 @NgModule({
