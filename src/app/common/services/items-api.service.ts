b2a51392c8b05d9af12365649760569eeb714dd9
Obtez <contact.davidbeer@gmail.com>
2022-04-03 21:09:34 +0200

    implemented POST route for items-api (creating new items and saving to database)

--- src/app/common/services/items-api.service.ts
+++ src/app/common/services/items-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs";
+import {Item} from "../models/item";
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,14 @@ export class ItemsApiService {
         return result;
       })
     );
+  }
+
+  saveNewItem(item: Item) {
+    return this.http.post(this.baseUrl + '/add', item).pipe(
+      map(result => {
+        return result;
+      })
+    )
   }
 
 }
