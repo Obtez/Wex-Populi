e4d8b8e4b09a83d3c81c5a1a9ead08ba48586d94
Obtez <contact.davidbeer@gmail.com>
2022-04-03 22:54:55 +0200

    routing for "/fighters" -> fightersComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {RecruitmentComponent} from "./features/recruitment/recruitment.component
 import {FactionsComponent} from "./features/factions/factions.component";
 import {ItemsComponent} from "./features/items/items.component";
 import {AdminComponent} from "./admin/admin.component";
+import {FightersComponent} from "./features/fighters/fighters.component";
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'recruitment', component: RecruitmentComponent },
   { path: 'factions', component: FactionsComponent },
   { path: 'items', component: ItemsComponent },
+  { path: 'fighters', component: FightersComponent },
 
   // ADMIN ROUTE
   { path: 'admin', component:  AdminComponent}
