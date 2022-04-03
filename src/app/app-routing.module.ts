88074650e296afafecfa3e1b31fd798a4a620848
Obtez <contact.davidbeer@gmail.com>
2022-04-03 18:49:13 +0200

    added "/items" path to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {StartScreenComponent} from "./features/start-screen/start-screen.compone
 import {DashboardComponent} from "./features/dashboard/dashboard.component";
 import {RecruitmentComponent} from "./features/recruitment/recruitment.component";
 import {FactionsComponent} from "./features/factions/factions.component";
+import {ItemsComponent} from "./features/items/items.component";
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'recruitment', component: RecruitmentComponent },
-  { path: 'factions', component: FactionsComponent }
+  { path: 'factions', component: FactionsComponent },
+  { path: 'items', component: ItemsComponent }
 ];
 
 @NgModule({
